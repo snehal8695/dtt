7d689984dbfb5d298d544754797975bbe4af73d2
Mayu <Mayu@Mayank>
2020-01-01 15:14:41 +0000

    changes in dbconnection

--- simple/src/connection/dbconnection.java
+++ simple/src/connection/dbconnection.java
@@ -8,7 +8,7 @@ public class dbconnection {
 	public static Connection getConnection() {
 		Connection con=null;
 	try {
-		Class.forName("com.mysql.cj.jdbc.Driver");
+		Class.forName("com.mysql.jdbc.Driver");
 		System.out.println("Driver Loaded");
 		con  = DriverManager.getConnection("jdbc:mysql://localhost:3306/defect_data?serverTimezone=UTC", "root", "");	
 		
